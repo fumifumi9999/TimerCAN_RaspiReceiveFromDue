72350ec6ad0387349c6adca88741a2b52784a86c
fumi <oobayashif@yamaha-motor.co.jp>
2017-01-10 14:05:00 +0900

    CAN受信関数化OK

--- TimerCAN_RaspiReceiveFromDue.py
+++ TimerCAN_RaspiReceiveFromDue.py
@@ -4,8 +4,7 @@ import RPi.GPIO as GPIO
 import time
 import os
 import can
-import queue
-from threading import Thread
+from multiprocessing import Process, Queue
 
 count = 0
 os.system("sudo /sbin/ip link set can0 up type can bitrate 500000")
@@ -19,42 +18,45 @@ triggerInputPin = 4
 plus = 0
 
 def can_rx_task():
-    while True:
-        message = bus.recv()
-        q.put(message)            # Put message into queue
+        while True:
+                message = bus.recv()
+                q.put(message)            # Put message into queue
 
-q = queue.Queue()
-t = Thread(target = can_rx_task)    # Start receive thread
+q = Queue()
+t = Process(target = can_rx_task)    # Start receive thread
+t .deamon = True
 t.start()
 
-def callbackFromDue10ms(triggerInputPin):
-    global plus
-    global count
+class CANErrorMessage:
+        def __init__(self):
+                self.arbitration_id = 0xFFFF
+                self.dlc = 8
+                self.data = [0xFF]*8
+                self.extended_id = False
+                self.timestamp = float(0)
 
-#    message = bus.recv()    # Wait until a message is received.
-#    if message.arbitration_id == 0x401:
-#        c = '{0:f} {1:x} {2:x} '.format(message.timestamp, message.arbitration_id, message.dlc)
-#        s=''
-#        for i in range(message.dlc ):
-#            s +=  '{0:x} '.format(message.data[i])
-#        print(plus, ' {}'.format(c+s))
+def getCANMessage(ID):
+        while q.empty() != True:    # Check if there is a message in queue
+                message = q.get()
+                if message.arbitration_id == ID:
+                        return message
+        return CANErrorMessage()
 
-# spread q.message to search the number that I want!!!!!!!!!!!!!
-    if q.empty() != True:    # Check if there is a message in queue
-        message = q.get()
-        c = '{0:f} {1:d} {2:x} {3:x} '.format(message.timestamp,count, message.arbitration_id, message.dlc)
+def printCANMessage(message):
+        c = '{0:f} {1:x} {2:x} '.format(message.timestamp, message.arbitration_id, message.dlc)
         s=''
         for i in range(message.dlc ):
-            s +=  '{0:x} '.format(message.data[i])
-        outstr = c+s
-        print('\r {} qsize:{}       '.format(outstr,q.qsize()),end ='') # Print data and queue size on screen
-        count += 1
+                s +=  '{0:x} '.format(message.data[i])
+        print(plus, ' {}'.format(c+s))
 
+def callbackFromDue10ms(triggerInputPin):
+        global plus
 
-    #-------receive--------
+        message = getCANMessage(0x407)
+        printCANMessage(message) #Id = ffff is no found Id error message
 
-    plus += 1
-    #print(plus)
+        plus += 1
+        #print(plus)
 
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(triggerInputPin, GPIO.IN)
